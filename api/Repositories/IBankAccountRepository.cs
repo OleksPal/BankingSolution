803162cf0300df695fdf349c7c85cf0c624569a9
User <oleksandr.palamarchuk1310@gmail.com>
2024-11-18 19:06:22 +0200

    Add the repository interface

---
+++ api/Repositories/IBankAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿using api.Models;
+
+namespace api.Repositories
+{
+    public interface IBankAccountRepository
+    {
+        Task<ICollection<BankAccount>> GetAll();
+        Task<BankAccount> GetByNumber(string accountNumber);
+        Task<BankAccount> Insert(BankAccount bankAccount);
+        Task<BankAccount> Update(BankAccount bankAccount);
+    }
+}
