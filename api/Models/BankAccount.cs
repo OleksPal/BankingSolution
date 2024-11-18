692adb5693a94525ff19a9a9adec9931813ca792
User <oleksandr.palamarchuk1310@gmail.com>
2024-11-18 18:16:53 +0200

    Add the BankAccount model

---
+++ api/Models/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Models
+{
+    public class BankAccount
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string AccountNumber { get; set; }
+
+        [Required]
+        [Range(0.0, double.PositiveInfinity)]
+        public decimal Balance { get; set; }
+    }
+}
