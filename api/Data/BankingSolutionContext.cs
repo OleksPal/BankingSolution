aade72d670cb2d68255acf594f5488ce44e3ed54
User <oleksandr.palamarchuk1310@gmail.com>
2024-11-18 18:22:10 +0200

    Add the BankingSolution's context

---
+++ api/Data/BankingSolutionContext.cs
@@ -0,0 +1,12 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class BankingSolutionContext : DbContext
+    {
+        public BankingSolutionContext(DbContextOptions<BankingSolutionContext> options) : base(options) { }
+
+        public DbSet<BankAccount> BankAccounts { get; set; }
+    }
+}
