bee2b46eb1f4c0ed4bc30a303273414daed9ccc4
User <oleksandr.palamarchuk1310@gmail.com>
2024-11-19 22:36:31 +0200

    Add the BankAccount dto and a mapper for it

---
+++ api/Mappers/BankAccountMappers.cs
@@ -0,0 +1,16 @@
+﻿using api.Dtos;
+
+namespace api.Mappers
+{
+    public static class BankAccountMappers
+    {
+        public static BankAccountDto ToBankAccountDto(this BankAccountDto bankAccountDto) 
+        {
+            return new BankAccountDto
+            {
+                AccountNumber = bankAccountDto.AccountNumber,
+                Balance = bankAccountDto.Balance
+            };
+        }
+    }
+}
