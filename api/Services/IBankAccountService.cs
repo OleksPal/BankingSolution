0706caa4f49b26cbad57199197c92def4924bc8e
User <oleksandr.palamarchuk1310@gmail.com>
2024-11-19 20:57:20 +0200

    Add the service interface

---
+++ api/Services/IBankAccountService.cs
@@ -0,0 +1,14 @@
+﻿using api.Models;
+
+namespace api.Services
+{
+    public interface IBankAccountService
+    {
+        Task<ICollection<BankAccount>> GetAll();
+        Task<BankAccount> GetBankAccountByNumber(string number);
+        Task<BankAccount> CreateAccount(decimal balance);
+        Task<BankAccount> Deposit(string number, decimal amount);
+        Task<BankAccount> Withdraw(string number, decimal amount);
+        Task<BankAccount> Transfer(string senderNumber, string recipientNumber, decimal amount);
+    }
+}
