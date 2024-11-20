d4f6a501a24fffcedfb6d7aaca30b1807dcf76f3
User <oleksandr.palamarchuk1310@gmail.com>
2024-11-20 16:51:45 +0200

    Add BankAccount controller tests

---
+++ tests/Controller tests/BankAccountControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using api.Controllers;
+using api.Dtos;
+using api.Services;
+using Microsoft.AspNetCore.Mvc;
+using tests.Helpers;
+
+namespace tests.Controller_tests
+{
+    public class BankAccountControllerTests
+    {
+        protected readonly BankAccountController _bankAccountController;
+
+        public BankAccountControllerTests()
+        {
+            var bankAccountService = Helper.GetRequiredService<IBankAccountService>()
+                ?? throw new ArgumentNullException(nameof(IBankAccountService));
+
+            _bankAccountController = new BankAccountController(bankAccountService);
+        }
+
+        #region GetAll
+        [Fact]
+        public async Task GetAll_ReturnsAccountsList()
+        {
+            // Act
+            var actionResult = await _bankAccountController.GetAll();
+
+            // Assert
+            var okResult = actionResult as OkObjectResult;
+            var accountList = okResult.Value as ICollection<BankAccountDto>;
+            Assert.NotEmpty(accountList);
+        }
+        #endregion
+    }
+}
