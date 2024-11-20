edf8837f1442f480365b6d645af49629bd3ddb0a
User <oleksandr.palamarchuk1310@gmail.com>
2024-11-20 14:54:45 +0200

    Add necessary helpers for testing

---
+++ tests/Repository tests/BankAccountRepositoryTests.cs
@@ -0,0 +1,33 @@
+﻿using api.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tests.Helpers;
+
+namespace tests.Repository_tests
+{
+    public class BankAccountRepositoryTests
+    {
+        protected readonly IBankAccountRepository _bankAccountRepository;
+
+        public BankAccountRepositoryTests()
+        {
+            _bankAccountRepository = Helper.GetRequiredService<IBankAccountRepository>()
+                ?? throw new ArgumentNullException(nameof(IBankAccountRepository));
+        }
+
+        #region GetAll
+        [Fact]
+        public async Task GetAll_ReturnsAccountsList()
+        {
+            // Act
+            var accountList = await _bankAccountRepository.GetAll();
+
+            // Assert
+            Assert.NotEmpty(accountList);
+        }
+        #endregion
+    }
+}
